b1bcecbde5cfdc83a27d8ea21dedadf3d69903cb
CheolWoong <bluesky4381@naver.com>
2018-02-20 14:47:23 +0900

    firstSection 퍼블리싱 완료

--- src/component/main/search_section.js
+++ src/component/main/search_section.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 
-const category_name = ["레슨", "홈/리빙", "이벤트", "비즈니스", "디자인/개발", "건강/미용", "기타"];
-const category_icon = [ "glyphicon glyphicon-user", 
-   "glyphicon glyphicon-home", "glyphicon glyphicon-list-alt", "glyphicon glyphicon-level-up", 
-   "glyphicon glyphicon-tasks", "glyphicon glyphicon-heart-empty", "glyphicon glyphicon-th" ];
+const accum_data = [
+    { name: "누적 요청서", num: "510,000", unit: "개"},
+    { name: "등록된 고수", num: "100,000", unit: "명"},
+    { name: "평균 리뷰별점", num: "4.5", unit: "/5점"},
+]
+
+const category_data = [
+    { icon: "glyphicon glyphicon-user", name: "레슨" }, 
+    { icon: "glyphicon glyphicon-home", name: "홈/리빙" },
+    { icon: "glyphicon glyphicon-list-alt", name: "이벤트" },
+    { icon: "glyphicon glyphicon-level-up", name: "비즈니스" },
+    { icon: "glyphicon glyphicon-tasks", name: "디자인/개발" },
+    { icon: "glyphicon glyphicon-heart-empty", name: "건강/미용" },
+    { icon: "glyphicon glyphicon-th", name: "기타" } 
+]
 
    
 export default class SearchSection extends Component {
 
-    renderIcon(category_icon, category_name){
+    renderAccumInfo({name, num, unit}){
+        return(
+            <div key={name} className="category">
+                <div>{name}</div>
+                <div>
+                    <span className="accumulated_num">{num}</span> {unit}
+                </div>
+            </div>
+        )
+    }
+
+    renderIcon({icon, name}){
         return (
-            <div align="center">
-               <span class={category_icon} aria-hidden="true"></span>
-               <p>{category_name}</p>
+            <div key={name} className="icon_category">
+               <span className={icon} aria-hidden="true"></span>
+               <p>{name}</p>
             </div>
         );
     }
@@ -20,6 +42,7 @@ export default class SearchSection extends Component {
     render(){
         return(
             <div>
+                {/* 네비게이션 바 */}
                 <div className="first_section_nav">
                     <a href="">
                         <img className="brand_logo" src="../../../img/brand_logo.png" alt="brandImage"/>
@@ -32,6 +55,7 @@ export default class SearchSection extends Component {
                     </span>
                 </div>
 
+                {/* 검색 바 */}
                 <div className="first_section_search" align="center">
                     <h1 id="first_section_search_word">숨고가 딱! 맞는 고수를 소개해 드려요</h1>
                     <form>
@@ -44,29 +68,14 @@ export default class SearchSection extends Component {
                     </form>
                 </div>
 
+                {/* 누적 정보 */}
                 <div id="accumulated_info" align="center">
-                   <div className="category">
-                        <div>누적 요청서</div>
-                        <div>
-                            <span className="accumulated_num">Num</span> 개
-                        </div>
-                   </div>
-                    <div className="category">
-                        <div>등록된 고수</div>
-                        <div>
-                            <span className="accumulated_num">Num</span> 명
-                        </div>
-                   </div>
-                   <div className="category">
-                        <div>평균 리뷰별점</div>
-                        <div>
-                            <span className="accumulated_num">Num</span> 점
-                        </div>
-                   </div>
+                   {accum_data.map( data => this.renderAccumInfo(data) )}
                 </div>
 
-                <div>
-                    {this.renderIcon()}
+                {/* 아이콘 표시 */}
+                <div className="background_grey" align="center">     
+                    {category_data.map( data => this.renderIcon(data) )}
                 </div>
             </div>
         );
